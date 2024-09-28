c7c8a3b3cf0684489f167ba1f54129cbc7649224
Oliver Cupitt <ollicupitt1999@gmail.com>
2024-09-28 14:41:47 +0200

    Add project files.

---
+++ Uppgift 1/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+
+
+
+
+
+
+        string input = "29535123p48723487597645723645";
+
+
+        // översätting till char så att man kan välja individuella characterer
+        char[] charArray = input.ToCharArray();
+        // length för while break
+        int x = charArray.Length;
+        int i = 0;
+
+
+        do
+        {
+            if (char.IsDigit(charArray[i]))
+            {
+                char startchar = charArray[i];
+                bool stringcheck = true;
+
+                for (int j = i + 1; j < x; j++)
+                {
+                    if (char.IsDigit(charArray[j]))
+                    {
+                        if (charArray[j] == startchar)
+                        {
+                            for (int k = i + 1; k < j; k++)
+                            {
+                                if (charArray[k] == startchar)
+                                {
+                                    stringcheck = false;
+                                    break;
+                                }
+                            }
+                            if (stringcheck = true)
+                            {
+                                Console.WriteLine($"{input.Substring(i, j - i + 1)}");
+                            }
+                            break;
+                        }
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+
+            }
+            i++;
+        }
+        while (x >= i);
+
+
+       
+       
+
+        
+    }
+    public void characterhighlight(string highlight)
+    { 
+    
+    }
+
+}
---
+++ Uppgift test/Program.cs
@@ -0,0 +1,5 @@
+﻿    
+
+Console.Write($"{input.Substring(0,i);
+Console.Write($"{input.Substring(i, j - i + 1)}");
+Console.Write($"{input.Substring(i + 1, x)}");
