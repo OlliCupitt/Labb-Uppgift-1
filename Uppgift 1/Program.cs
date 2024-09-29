15756378dd6df428031e5030f23c31f79dff5bca
Oliver Cupitt <ollicupitt1999@gmail.com>
2024-09-29 18:20:00 +0200

    comments added to make the code more understandable. addded console.readline so a random sequence can be tested but i commented it out, if it wants to be tested remove // from CW and CR and add to string input

--- Uppgift 1/Program.cs
+++ Uppgift 1/Program.cs
@@ -1,92 +1,94 @@
 ﻿using System;
-using System.Security.Cryptography.X509Certificates;
+
 class Program
 {
     static void Main(string[] args)
     {
-
+        //Console.Write("input you chosen sequence then press enter: ");
+        //string input =Console.ReadLine();
         string input = "29535123p48723487597645723645";
 
-        // översätting till char så att man kan välja individuella characterer
+        // Convert string to char array to easily access individual characters.
         char[] charArray = input.ToCharArray();
-        // length för while break
+        // length för while break criteria 
+        // definiera variables som kommer behövas i senare loops
         int length = charArray.Length;
         int i = 0;
         long sum = 0;
         
 
 
-        do
+        do                                                          // do while loop för att välja första character i Array
         {
-            if (char.IsDigit(charArray[i]))
+            if (char.IsDigit(charArray[i]))                         // if statement to check if it is a digit, if not do loop jumps to next character in Array
             {
-                char startchar = charArray[i];
-                bool stringcheck = true;
+                char startchar = charArray[i];                      // saving first character and identifying it as startchar to use to compare later
+                bool stringcheck = true;                            // setting a stingcheck to use for identifying errors between i & j 
 
-                for (int j = i + 1; j < length; j++)
-                {
-                    if (char.IsDigit(charArray[j]))
+                for (int j = i + 1; j < length; j++)                // for loop to check startchar(i) against the next character in the array and so on (j++)
+                { 
+                    if (char.IsDigit(charArray[j]))                 // if statement to check that j is a digit for the for loop 
                     {
-                        if (charArray[j] == startchar)
+                        if (charArray[j] == startchar)              // if statement to compare startchar to j
                         {
-                            for (int k = i + 1; k < j; k++)
+                            for (int k = i + 1; k < j; k++)         // Check the digits between i and j to ensure no repeated startchar.
                             {
-                                if (charArray[k] == startchar)
-                                {
-                                    stringcheck = false;
+                                if (charArray[k] == startchar)      // checking startchar against k, if this is true the loop will set stringcheck to 
+                                {                                   // false and break the loop and restart.
+                                    stringcheck = false;            
                                     break;
                                 }
                             }
-                            if (stringcheck)
-                            {
-                                
-                                characterhighlight(i, j, input);
-                               string selectedsubstring = input.Substring(i, j - i + 1);
+                            if (stringcheck)                                                   // If no issues were found, highlight and sum the valid sequence.
+                            {                                                 
+                                             
+                                characterhighlight(i, j, input);                               // calling on the methods characterhighlight & Selectedsum  
+                               string selectedsubstring = input.Substring(i, j - i + 1);       // Extract the valid substring and add it to the sum.
 
                                 sum += SelectedSum(selectedsubstring);
                               
                                
                             }
-                            break;
+                            break;                                                     // break after a correct sequence is found
                         }
                     }
-                    else
+                    else                                                               // if j is not a digit the loop is broken
                     {
                         break;
                     }
                 }
             }
-            i++;
-        }
-        while (i < length);
-        Console.WriteLine($"total sum of all highlighted sections {sum}");
+            i++;                                                                   // moving along to the next char in array afer all other loops and checks have been performed 
+        }                                                                                  
+        while (i < length);                                                        // length used as loop-breaking condition  
+        Console.WriteLine();                                                       // print extra line to make it easier to read 
+        Console.WriteLine($"total sum of all highlighted sections {sum}");         // sum print after the do/while loop has closed 
     }
-    public static void characterhighlight(int i, int j, string input)
+    public static void characterhighlight(int i, int j, string input)              // creating the method characterhighlights      
     {
-        string Selectedhighlight = ($"{input.Substring(i, j - i + 1)}");
-        Console.Write($"{input.Substring(0, i)}");
-        Console.ForegroundColor = ConsoleColor.Red;
-        Console.Write(Selectedhighlight);
-        Console.ResetColor();
-        Console.WriteLine($"{input.Substring(j, input.Length - j)}");
+        string Selectedhighlight = ($"{input.Substring(i, j - i + 1)}");           // casting a string between i & j from the char array 
+        Console.Write($"{input.Substring(0, i)}");                                 // printing the part before highlighted sequence 0 - i
+        Console.ForegroundColor = ConsoleColor.Red;                                // colour change (highlight)
+        Console.Write(Selectedhighlight);                                          // sequence print 
+        Console.ResetColor();                                                      // colour reset to not change the rest of the console colour
+        Console.WriteLine($"{input.Substring(j, input.Length - j)}");              // last part of string print j - x
 
 
 
     }
 
-    public static long SelectedSum(string selectedHighlight)
+    public static long SelectedSum(string selectedHighlight)                       // creating of the method Selectedsum 
     {
 
 
-        if (long.TryParse(selectedHighlight, out long number))
-        {
+        if (long.TryParse(selectedHighlight, out long number))                     // converting the chosen sequence from the loop to a long
+        {                                                                          
 
-            return number;
+            return number;                                                         // sum addition
         }
         
-        return 0;
+        return 0;                                                                  // if it can not be parsed the sum will retun to zero
     }
-     
 }
 
 
